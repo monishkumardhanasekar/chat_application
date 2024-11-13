560abdab7f31bd99a54fc0cccfecbb2bacc191b4
monishkumardhanasekar <145507012+monishkumardhanasekar@users.noreply.github.com>
2024-11-13 13:42:17 -0500

    updated Readme

--- src/main/java/com/example/chat_application/config/WebSocketConfig.java
+++ src/main/java/com/example/chat_application/config/WebSocketConfig.java
@@ -1,28 +1,3 @@
-// package com.example.chat_application.config;
-
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.lang.NonNull; // Import NonNull annotation
-// import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-// import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-// import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-// import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-
-// @Configuration
-// @EnableWebSocketMessageBroker
-// public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-//     @Override
-//     public void configureMessageBroker(@NonNull MessageBrokerRegistry config) { // Add NonNull annotation
-//         config.enableSimpleBroker("/topic"); // Enables a broker to broadcast messages to topics
-//         config.setApplicationDestinationPrefixes("/app"); // Prefix for message mappings
-//     }
-
-//     @Override
-//     public void registerStompEndpoints(@NonNull StompEndpointRegistry registry) {
-//         registry.addEndpoint("/ws-chat").withSockJS(); // WebSocket endpoint
-//     }
-// }
-
 package com.example.chat_application.config;
 
 import org.springframework.context.annotation.Configuration;
